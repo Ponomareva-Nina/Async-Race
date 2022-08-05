dbf7be644bddcd893048908d38339737511b32c4
Nina Ponomareva <nina.9412@mail.ru>
2022-08-05 21:33:26 +0300

    init: start task

---
+++ application/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const sass = require('sass');
+
+module.exports = {
+    context: path.resolve(__dirname,"src"),
+    mode: "development",
+    entry: {
+        main: "./scripts/app.js",
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "../public"),
+        assetModuleFilename: "./assets/[name][ext]"
+    },
+    
+    resolve: {
+        alias: {
+            "@fonts": path.resolve(__dirname, "src/assets/fonts"),
+            "@img": path.resolve(__dirname, "src/assets/img")
+        }
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./index.html"
+        }),
+        new CleanWebpackPlugin()
+    ],
+    //loaders: 
+    module: {
+        rules: [
+            {test: /\.css$/, 
+            use: ['style-loader', 'css-loader']},
+
+            {test: /\.(jpg|JPG|jpeg|png|svg|gif)$/,  
+            type: "asset/resource"},
+
+            {test: /\.(ttf|woff|woff2|eot)$/, 
+            type: "asset/resource"},
+            
+            {test: /\.s[ac]ss$/i,
+            use: ["style-loader","css-loader","sass-loader"]
+            }
+        ]
+    }
+}
